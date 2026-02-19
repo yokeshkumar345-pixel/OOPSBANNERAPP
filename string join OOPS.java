e5600d96734c011373c72260864507ad803a84bf
yokeshkumar345-pixel <yokeshkumar345@gmail.com>
2026-02-19 10:29:49 +0530

    Add files via upload

---
+++ string join OOPS.java
@@ -0,0 +1,11 @@
+public class OOPS{
+    public static void main(String[] args) {
+        System.out.println(String.join("   ***     " , "   ***     " , "******   " , "  ****  "));
+        System.out.println(string.join(" **   **   " , " **   **   " , "**   **  " , "**    **"));
+        System.out.println(string.join("**     **  " , "**     **  " , "**   **  " , "**      "));
+        System.out.println(string.join("**     **  " , "**     **  " , "******   " , " ****** "));
+        System.out.println(string.join("**     **  " , "**     **  " , "**       " , "      **"));
+        System.out.println(string.join(" **  **    " , " **   **   " , "**       " , "**    **"));
+        System.out.println(string.join("   ***     " , "   ***     " , "**       " , "  ****  "));
+    }
+}
