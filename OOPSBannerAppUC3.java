c763b3a133507852d79939accd5bfefbc41d0333
YOKESHKUMAR <yokeshkumar345@gmail.com>
2026-03-07 11:10:04 +0530

    coded to print OOPS using string join

---
+++ OOPSBannerAppUC3.java
@@ -0,0 +1,11 @@
+public class OOPSBannerAppUC3{
+    public static void main (String[] args){
+        System.out.println(String.join(" ", "   ***    ", "   ***   ", " ****** ", "   ***** "));
+        System.out.println(String.join(" ", " **    ** ", " **   ** ", " **   **", " **      "));
+        System.out.println(String.join(" ", "**      **", "**     **", " **   **", "**       "));
+        System.out.println(String.join(" ", "**      **", "**     **", " ****** ", "  ****** "));
+        System.out.println(String.join(" ", "**      **", "**     **", " **     ", "      ** "));
+        System.out.println(String.join(" ", " **    ** ", " **   ** ", " **     ", "     **  "));
+        System.out.println(String.join(" ", "   ***    ", "   ***   ", " **     ", " ****    "));
+    }
+}
